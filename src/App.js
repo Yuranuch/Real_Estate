2b91166f494c6bf039e6fbbe9d85c719d4eba8f8
Yuranuch <yura477573@mail.ru>
2019-11-14 23:40:50 +0300

    header done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="main_wrap">
+            <div className="header_wrap">
+            <div className="header">
+                <a href="" className="header_logo">
+                    <span className="header_logo_text">Realestate Company</span>
+                    <span className="header_logo_smallText">Lorem Ipsum is simply dummy text</span>
+                </a>
+                <div className="header_phone">
+                    <span>Call us: (214) 227-7669</span>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
